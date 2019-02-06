20261c8e33f92c3bfd965a6be07869f59228aea1
Moroni Stubbe <moroni.stubbe@icloud.com>
2019-02-06 11:09:19 +0100

    first commit

---
+++ Periode 3/Realiseren en Ontwerpen/1 - OOP/Opdracht 1 - Introductie OOP.php
@@ -0,0 +1,19 @@
+<?php
+
+//vroeger gebruikten ze arrays met variabelen
+
+class Person
+{
+    public $name;
+    public $age;
+}
+
+$person1 = new Person();
+$person1->name = "Moroni";
+$person1->age = 18;
+
+$person2 = new Person();
+$person2->name = "Aaron";
+$person2->age = 17;
+
+?>
